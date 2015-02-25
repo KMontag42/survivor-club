14bf9d4a640b520581180d4502d1403c98ff4428
Kyle Montag <kmontag@skillz.com>
2015-02-24 20:39:00 -0500

    whoop lol

--- app/controllers/game_day_controller.rb
+++ app/controllers/game_day_controller.rb
@@ -4,7 +4,7 @@ class GameDayController < ApplicationController
   def index
     @episode = Episode.where("air_date >= '#{Date.today}'").
                        sort { |a, b| a.number <=> b.number }.first
-    @players = @episode.season.picks.map(&:user)
+    @players = @episode.season.picks.map(&:user).uniq
   end
 
 end

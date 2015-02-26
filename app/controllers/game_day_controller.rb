35046c4ca38e6e979fa666f774e8b8696c3321be
Kyle Montag <kmontag@skillz.com>
2015-02-25 19:09:31 -0500

    slight copy change

--- app/controllers/game_day_controller.rb
+++ app/controllers/game_day_controller.rb
@@ -2,7 +2,8 @@ class GameDayController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @episode = Episode.where("air_date >= '#{Date.today}'").
+    # -6 hours to account for the time zone of the server
+    @episode = Episode.where("air_date >= '#{Date.today - 6.hour}'").
                        sort { |a, b| a.number <=> b.number }.first
     @players = @episode.season.picks.map(&:user).uniq
   end

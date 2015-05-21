e575bebdccf02b63aa725191ca27ef8c693a15ab
Kyle Montag <kmontag@skillz.com>
2015-05-21 16:35:08 -0400

    bunch of updates to the homepage

--- app/models/second_chance_potential.rb
+++ app/models/second_chance_potential.rb
@@ -9,7 +9,12 @@
 #  updated_at :datetime
 #  gender     :string(255)
 #
-
+# vote winners:
+# ["Vytas Baskauskas", "Cierra Eastin", "Peih-Gee Law", "Keith Nale",
+# "Jeff Varner", "Kimmi Kappenberg", "Kelly Wiglesworth", "Shirin Oskooi",
+# "Kelley Wentworth", "Monica Padilla", "Kass McQuillen", "Abi-Maria Gomes",
+# "Andrew Savage", "Terry Deitz", "Woo Hwang", "Spencer Bledsoe", "Joe Anglim",
+# "Jeremy Collins", "Tasha Fox", "Stephen Fishbach"]
 class SecondChancePotential < ActiveRecord::Base
 
   has_many :second_chance_votes

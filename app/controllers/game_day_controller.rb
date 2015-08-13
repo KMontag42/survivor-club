0c6b7da06d12e09a7126d1f417f9bec271de596e
kmontag <kmontag@skillz.com>
2015-08-13 15:02:44 -0400

    new game day page scaffold

--- app/controllers/game_day_controller.rb
+++ app/controllers/game_day_controller.rb
@@ -1,7 +1,11 @@
 class GameDayController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
+
+  end
+
+  def old_index
     # -6 hours to account for the time zone of the server
     @episode = Episode.where("air_date >= '#{Date.today - 6.hour}'").
                        sort { |a, b| a.number <=> b.number }.first

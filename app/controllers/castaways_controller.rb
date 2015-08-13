20baccb680623c73f899b09344f7670c882ad4fc
kmontag <kmontag@skillz.com>
2015-08-13 16:37:40 -0400

    castaway CRUD forms

--- app/controllers/castaways_controller.rb
+++ app/controllers/castaways_controller.rb
@@ -11,15 +11,43 @@ class CastawaysController < ApplicationController
     @castaway = Castaway.find(params[:id])
   end
 
+  def new
+    render layout: 'center_form'
+  end
+
   def edit
     @castaway = Castaway.find(params[:id])
   end
 
   def create
-    @castaway = Castaway.new
+    @castaway = Castaway.new(allowed_params)
+    if @castaway.save
+      flash[:success] = 'Castaway created!'
+      redirect_to admin_path
+    else
+      flash[:danger] = @castaway.errors
+      respond_with @castaway do |format|
+        format.any
+      end
+    end
   end
 
   def update
     @castaway = Castaway.find(params[:id])
+    if @castaway.update(allowed_params)
+      flash[:success] = 'Castaway updated!'
+      redirect_to admin_path
+    else
+      flash[:danger] = @castaway.errors
+      respond_with @castaway do |format|
+        format.any
+      end
+    end
+  end
+
+  protected
+
+  def allowed_params
+    params.require(:castaway).permit(:id, :name, :season_id, :original_tribe_id)
   end
 end

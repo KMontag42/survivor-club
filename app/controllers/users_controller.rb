98efd0eadd76cd631cc8932237597a0d6bd9d381
kmontag <kmontag@skillz.com>
2015-09-15 22:14:26 -0400

    add strong param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :show_spoilers)
+    params.require(:user).permit(:first_name, :last_name, :email,
+                                 :show_spoilers, :picture_url)
   end
 end

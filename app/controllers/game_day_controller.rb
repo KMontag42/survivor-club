b9305345b763ae843b8c8a980366b3fc906a9f24
kmontag <kmontag@skillz.com>
2015-09-30 10:34:30 -0400

    update gameday controller to grab an episode

--- app/controllers/game_day_controller.rb
+++ app/controllers/game_day_controller.rb
@@ -2,6 +2,9 @@ class GameDayController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # -6 hours to account for the time zone of the server
+    @episode = Episode.where("air_date >= '#{Date.today - 6.hour}'").
+        sort { |a, b| a.number <=> b.number }.first
     @players = User.joins(:picks).where.not(picks: {user_id: nil}).uniq
   end
 

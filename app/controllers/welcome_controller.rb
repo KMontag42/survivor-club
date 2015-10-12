3d708652a2e006e050b88786e485eb47f9be2628
kmontag <kmontag@skillz.com>
2015-10-12 19:24:04 -0400

    updates to the landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,9 @@ class WelcomeController < ApplicationController
   def index
     @show_spoilers = current_user.try(:show_spoilers)
     @recent_vote_outs = VoteOut.all.reverse_order[0..2]
+
+    if current_user
+      # redirect_to game_day_path
+    end
   end
 end
